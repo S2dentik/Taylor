abb46d6efb5800089e40669a4c2a1680038e9581
Alexandru Culeva <alexandru.culeva@ellation.com>
2017-04-18 00:53:54 +0300

    Update to Swift 3.1
    * Add 'ExcludeFileReader' framework as Carthage dependency.

--- test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-CONFIGURATION="Release"
-BUILD_DIR="./build"
-TEST_CMD="xcodebuild test -scheme Taylor -sdk macosx -enableCodeCoverage YES -derivedDataPath ${BUILD_DIR}"
-
-# Run without xcpretty due to a bug that leads to incorrect coverage data ¯\_(ツ)_/¯
-eval "$TEST_CMD"
